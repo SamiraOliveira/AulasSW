b2ab90f5feb6c38ef443a659ab6cad53ed807242
Samira de Melo Oliveira <80074956+SamiraOliveira@users.noreply.github.com>
2022-03-18 12:32:12 -0300

    Add files via upload

---
+++ aula_18_03_22/exemplo_02.php
@@ -0,0 +1,15 @@
+<?php
+    if($nota>6){
+        //comando para verdadeiro
+        echo "APROVADO";
+    }
+    else {
+        //comando para falso
+        if($nota<5){
+            echo"REPROVADO";
+        }
+        else {
+            echo "RECUPERAÇÃO";
+        }
+
+?>
---
+++ aula_18_03_22/exercicio_01.php
@@ -0,0 +1,18 @@
+<?php
+    $idade = 17;
+    $hab = true;
+    if($idade>=18){
+        echo "Você tem a idade certa para dirigir!";
+    }
+    else {
+        echo "Desculpe, você ainda não tem a idade certa para dirigir...";
+    }
+ 
+    if($hab =="sim"){
+        echo " Parabéns, você foi aprovado!!!";
+    }
+    else {
+        echo " Infelizmente você foi reprovado.";
+    }
+
+?>
