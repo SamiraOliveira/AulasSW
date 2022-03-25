429923a3d43c26be4fd02e6ad7360e720cede7ab
Samira de Melo Oliveira <80074956+SamiraOliveira@users.noreply.github.com>
2022-03-25 12:20:54 -0300

    Update exemplo_03.php

--- aula_25_02_22/exemplo_03.php
+++ aula_25_02_22/exemplo_03.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,13 +8,13 @@
 </head>
 <body>
     <?php
-        $nome = "Raíssa";
+        $nome = "Samira";
     ?>
     <h1>oii</h1>
     <p>aaaa</p>
     <h2>
         <?php
-            echo 'Olá'.  $nome;
+            echo 'Olá'. $nome;
         ?>
         <?php
             echo "Olá $nome";
