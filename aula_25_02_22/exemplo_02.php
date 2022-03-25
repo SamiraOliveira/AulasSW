1e64bd3935962b7ebebd5b8fdb290e4268887ce3
Samira de Melo Oliveira <80074956+SamiraOliveira@users.noreply.github.com>
2022-03-25 12:20:15 -0300

    Update exemplo_02.php

--- aula_25_02_22/exemplo_02.php
+++ aula_25_02_22/exemplo_02.php
@@ -1,7 +1,7 @@
 <?php
     // echo serve para escrever coisas na tela. Depois coloque aspas para o texto escrito.]
     // Sempre termine com ;
-    echo "Oi lindezas";
+    echo "Olá Mundo!!!";
 
 
 
