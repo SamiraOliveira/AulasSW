df1d28730196be3eb1e875bc0dce9ab0e14fe5f6
Samira de Melo Oliveira <80074956+SamiraOliveira@users.noreply.github.com>
2022-04-29 12:19:29 -0300

    Add files via upload

---
+++ aula_29_04_22/for/exemplo_1.php
@@ -0,0 +1,10 @@
+<?php 
+    echo "Contagem Progressiva <br> <br>";
+    for($cont= 0; $cont<10;$cont++){
+      echo"A variavel \$cont vale $cont <br>";
+    }
+    echo "<br> Contagem Regressiva <br> <br>";
+    for($cont=13;$cont>0;$cont--){
+      echo"A variavel \$cont vale $cont <br>";
+    }
+?>
